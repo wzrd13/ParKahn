1fe380a40a806098b9536e2f12c47875e548e364
wzrd13 <42070629+wzrd13@users.noreply.github.com>
2020-04-28 11:29:49 +0300

    first attempt to parallel the algorithm

--- par_khan.c
+++ par_khan.c
@@ -8,8 +8,8 @@
 
 //------- GLOBAL VARIABLES --------//
 struct Graph graph;
-struct Stack* stack;
-struct Stack* sorted;
+struct Stack* S;
+struct Stack* L;
 //--------------------------------//
 
 
@@ -21,30 +21,30 @@ struct Graph {
 
 struct Graph get_matrix(FILE *fp) {
 	
-	// create graph struct
+	// Create graph struct
 	struct Graph graph;
 
-	// vars for holding rows and cols
+	// Vars for holding rows and cols
 	int value1;
 	int value2;
 
-	// get number of nodes and edges
+	// Get number of nodes and edges
 	fscanf(fp, "%*d %d %d", &graph.num_nodes, &graph.num_edges);
 	//printf("%d %d\n", graph.num_nodes, graph.num_edges);
 
-	// allocate memory for matrix 2d array
+	// Allocate memory for matrix 2d array
 	graph.matrix = (int **)malloc(graph.num_nodes * sizeof(int *)); 
     for (int i=0; i<graph.num_nodes; i++) 
          graph.matrix[i] = (int *)malloc(graph.num_nodes * sizeof(int));
 
-     // intialize matrix array with 0;
+    // Intialize matrix array with 0;
 	for(int i = 0; i < graph.num_nodes; i++) {
 		for(int j = 0; j < graph.num_nodes; j++) {
 			graph.matrix[i][j] = 0;
 		}
 	}
 
-	// fill matrix
+	// Fill matrix
 	while(fscanf(fp, "%d %d", &value1, &value2) != EOF) {
 		graph.matrix[value1-1][value2-1] = 1;
 	}
@@ -52,7 +52,7 @@ struct Graph get_matrix(FILE *fp) {
 	return graph;
 }
 
-// check
+// Check
 int* set_node_degree(struct Graph graph) {
 
 	int *degree = malloc(sizeof(int)*graph.num_nodes);
@@ -69,51 +69,28 @@ int* set_node_degree(struct Graph graph) {
 	return degree;	
 }
 
-//Checks if the graph has edges. 
-bool graph_has_edges(int* degree, struct Graph graph) {
-	for(int i = 0; i < graph.num_nodes; i++) {
-		if(degree[i] > 0) { 
-			return true;
-		}
-	}
-	return false;
-}
-
-//Print Matrix
+// Print Matrix
 void print_matrix(struct Graph graph) {
-		printf("%-3c", 'X');
-		for(int i = 0; i < graph.num_nodes; i++) {
-			printf("%3d", i+1);
-		}
-		printf("\n\n");
+	printf("%-3c", 'X');
+	for(int i = 0; i < graph.num_nodes; i++) {
+		printf("%3d", i+1);
+	}
+	printf("\n\n");
 
-		 for(int i = 0; i < graph.num_nodes; i++) {
+	for(int i = 0; i < graph.num_nodes; i++) {
 			
-			printf("%-3d", i+1);
-			for(int j = 0; j < graph.num_nodes; j++) {
-					
-					//COLOURS!!! XD
-					if(graph.matrix[i][j]==0) printf("%3d", graph.matrix[i][j]);
-					else printf("\033[0;32m%3d\033[0m", graph.matrix[i][j]);
-					
-				}
-
-		 	printf("\n");
-		 }
+		printf("%-3d", i+1);
+		for(int j = 0; j < graph.num_nodes; j++) {					
+			//COLOURS!!! XD
+			if(graph.matrix[i][j]==0) printf("%3d", graph.matrix[i][j]);
+			else printf("\033[0;32m%3d\033[0m", graph.matrix[i][j]);				
+		}
+		printf("\n");
+	}
 }
 
 
-unsigned long get_time() {
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
-        unsigned long ret = tv.tv_usec;
-        ret /= 1000;
-        ret += (tv.tv_sec * 1000);
-        return ret;
-}
-
-
-void kahn_algorithm() {
+bool kahn_algorithm() {
 	
 	// Initialize node degrees
 	int *degree = set_node_degree(graph);
@@ -124,44 +101,65 @@ void kahn_algorithm() {
 	{
 		if (degree[i] == 0)
 		{
-			push(stack , i);
-			printf("PUSH: %d\n", i+1);
+			push(S , i);
 		}		
 	}
 
 	//while S is not empty
-	while(is_empty(stack) == false){
-		
-		//remove a node n from S
-		int node_n =  return_and_remove_head(stack);		
-		//add n to tail of L								
-		push(sorted, node_n);
-		
-		//for each node m with an edge e from n to m do
-		for(int i = 0; i < graph.num_nodes; i++){
-			if(graph.matrix[node_n][i] == 1){
-				graph.matrix[node_n][i] = 0;
-				degree[i]--;
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+			while(1){
+			
+				#pragma omp task
+				{
+					//remove a node n from S
+					int node_n =  return_and_remove_head(S);		
+					//add n to tail of L								
+					push(L, node_n);
+				
+					//for each node m with an edge e from n to m do
+					for(int i = 0; i < graph.num_nodes; i++){
+						if(graph.matrix[node_n][i] == 1){
+							
+							#pragma omp critical
+							{
+								graph.matrix[node_n][i] = 0;
+								degree[i]--;
+							}
+							
 
-				if(degree[i] == 0) {
-					push(stack, i);
+							if(degree[i] == 0) {
+								
+								#pragma omp critical
+								{
+									push(S, i);	
+								}
+								
+							}
+						}
+					}
+
+				}// End of task region
+
+				// Check if stack S is empty
+				#pragma omp taskwait
+				if(is_empty(S) == true) {
+					break;
 				}
 			}
-		}    
-	}
+		}
+	}// End of parallel region
 	
-	//if graph has edges then print "graph has at least one cycle"
-	if(graph_has_edges(degree, graph) == true){
-		red();
-		printf("\nGraph has at least one cycle\n");
-		reset();
+	
+	// Check if graph has remaining edges 
+	for(int i = 0; i < graph.num_nodes; i++) {
+		if(degree[i] > 0) { 
+			return false;
+		}
 	}
-	else{
-		green();
-		printf("\nThe following is in a topologically sorted order.\n");
-		reset();
-		print_stack_bot_to_top(sorted);
-	}
+	return true;
 }
 
 
@@ -178,13 +176,13 @@ int main(int argc, char *argv[]) {
 		strcpy(file, argv[1]);
 	}
 	else {
-		printf("Please type the path to the graph file\n");
+		printf("\nPlease type the path to the graph file\n");
 		return 1;
 	}	
 
 	// If file does not exists
 	if((fp = fopen(file, "r")) == NULL) {
-		printf("The file you specified does not exist\n");
+		printf("\nThe file you specified does not exist\n");
 		return 1;
 	}
 		
@@ -195,18 +193,23 @@ int main(int argc, char *argv[]) {
 	fclose(fp);
 
 	// Initialize the S Stack
-	stack =  init_stack();
-	// Initialize the L stack
-	sorted  =  init_stack();
+	S =  init_stack();
+	// Initialize the L S
+	L  =  init_stack();
 
 	//Run the Glorious Algorithm
-	kahn_algorithm();
-
-
-	// long end =  get_time() - start;
-	// yellow();
-	// printf("\nTime: %ld \n", end);
-	// reset();
+	if (kahn_algorithm() == false) {
+		red();
+		printf("Graph has at least one cycle\n");
+		reset();
+		exit(0);
+	}
+	else {
+		green();
+		printf("The following is in a topologically correct order.\n");
+		reset();
+		print_stack_bot_to_top(L);
+	}
 
 	return 0;
 }
