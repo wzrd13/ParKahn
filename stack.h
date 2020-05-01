77b1ce0a3f54620f9ba87e53854f729cf13430e0
wzrd13 <42070629+wzrd13@users.noreply.github.com>
2020-05-01 18:01:38 +0300

    concatinate stack method added to stack.h

--- stack.h
+++ stack.h
@@ -9,27 +9,67 @@ struct Node{
 
 struct Stack{
     struct Node* head;
+<<<<<<< HEAD
+    struct Node* tail;
+    int size;
+};
+
+=======
     int size;
 };
 
 
+>>>>>>> cb204fdb9c8b1c2356ee5156ea3bc254dd0109c4
 void red(){ printf("\033[0;31m"); }
 void green(){ printf("\033[0;32m");}
 void yellow(){ printf("\033[0;33m");}
 void reset(){ printf("\033[0m");}
 
 struct Stack* init_stack(){
+<<<<<<< HEAD
+    
+    // Create pointer to stack
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+    
+    // Initialize stack variables
+    stack->head = NULL;
+    stack->tail = NULL;
+    stack->size = 0;
+
+    // Return pointer
+=======
     struct Stack* stack = (struct Stack*)malloc(sizeof(struct Node));
     stack->head = NULL;
     stack->size = 0;
+>>>>>>> cb204fdb9c8b1c2356ee5156ea3bc254dd0109c4
     return stack;
 }
 
 void push(struct Stack* stack, int node_num){
+<<<<<<< HEAD
+    
+    // Create pointer to node
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+    // Get tail
+    if (stack->head == NULL)        
+    {
+        stack->tail = new_node;
+    }
+
+    // Initialize node values
+    new_node->node_num = node_num;
+    new_node->previous_node = stack->head;
+    
+    // Get head
+    stack->head = new_node;
+
+=======
     struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
     new_node->node_num = node_num;
     new_node->previous_node = stack->head;
     stack->head = new_node; 
+>>>>>>> cb204fdb9c8b1c2356ee5156ea3bc254dd0109c4
     stack->size++;
 }
 
@@ -41,6 +81,47 @@ bool is_empty(struct Stack* stack){
     else return false;
 }
 
+<<<<<<< HEAD
+int peek(struct Stack* stack){
+    if(is_empty(stack)){
+        return -1;
+    }
+    return stack->head->node_num;
+}
+
+int pop(struct Stack* stack){
+    
+    // Check if stack is empty
+    if(is_empty(stack)) return -1;
+    
+    // Get value
+    int n = stack->head->node_num;
+
+    // Free memory
+    struct Node* top = stack->head;
+
+    // Update stack
+    stack->head = stack->head->previous_node;
+    stack->size = stack->size - 1;
+    
+    free(top);
+    return n;
+}
+
+struct Stack* concatinate(struct Stack* A, struct Stack* B) {
+    
+    if(is_empty(B)) return A;
+    if(is_empty(A)) return B;
+
+    B->tail->previous_node = A->head;
+    A->head = B->head;
+    A->size = B->size + A->size;
+
+    return A;
+}
+
+
+=======
 bool pop(struct Stack* stack){
     
     if(is_empty(stack)) return false;
@@ -64,6 +145,7 @@ int return_and_remove_head(struct Stack* stack){
     pop(stack);
     return node_num;
 }
+>>>>>>> cb204fdb9c8b1c2356ee5156ea3bc254dd0109c4
 
 int get_stack_size(struct Stack* stack){
     return stack->size;
@@ -99,6 +181,56 @@ void print_stack_bot_to_top(struct Stack* stack){
     reset();
 }
 
+<<<<<<< HEAD
+
+//STACK PLAYGROUND
+int main(){
+
+    struct Stack* S =  init_stack();
+    struct Stack* C =  init_stack();
+
+    print_stack(S);
+
+    push(S ,1);
+    push(S ,2);
+    push(S ,3);
+    push(S ,4);
+    push(S ,5);
+
+
+    push(C ,11);
+    push(C ,10);
+    push(C ,10);
+    push(C ,10);
+    push(C ,22);
+
+    printf("stack S\n");
+    print_stack(S);
+
+    printf("stack C\n");
+    print_stack(C);
+
+    printf("stack S+C\n");
+    S = concatinate(S, C);
+
+
+    print_stack(S);
+
+    // printf("%d\n", S->head->node_num);
+
+    // printf("%d\n", S->tail->node_num);
+
+    // printf("pop %d\n", pop(S));
+
+    // printf("pop %d\n", pop(S));
+
+    // printf("%d\n", S->head->node_num);
+
+    // printf("%d\n", S->tail->node_num);
+
+    return 0;
+}
+=======
 /*
 //STACK PLAYGROUND
 int main(){
@@ -128,3 +260,4 @@ int main(){
     return 0;
 }
 */
+>>>>>>> cb204fdb9c8b1c2356ee5156ea3bc254dd0109c4
